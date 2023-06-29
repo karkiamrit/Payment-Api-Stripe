56ce893aff6f101f2d5ec9eb837cac3e93c9e360
karkiamrit <amrit.karki2073@gmail.com>
2023-06-29 10:44:42 +0545

    addiing ejs

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
 const express=require('express');
 const app=express();
 const dotenv=require('dotenv').config();
+const fs=require('fs');
 
 const stripeSecretKey=process.env.STRIPE_SECRET_KEY;
 const stripePublicKey=process.env.STRIPE_PUBLIC_KEY;
 
 app.set('view engine','ejs');
 app.use(express.static('public'));
-  
+
+app.get('/store', function(req,res){
+    fs.readFile('items.json',function(error,data){
+        if(error){
+            res.status(500).end();
+        }else{
+            res.render('store.ejs',{
+                items:JSON.parse(data)
+            });
+        }
+    })
+})
 
 app.listen(3000,()=>{
     console.log('server is running on port 3000');
